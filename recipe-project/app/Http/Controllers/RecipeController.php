83e23250cb6c3d65854a1c44116ce203642e232f
Yasu1025 <sillyjp25@gmail.com>
2024-11-18 15:03:10 +0900

    [wip] just get recipes for createing view for now

--- recipe-project/app/Http/Controllers/RecipeController.php
+++ recipe-project/app/Http/Controllers/RecipeController.php
@@ -3,15 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recipe;
 
 class RecipeController extends Controller
 {
+    public function home()
+    {
+        // get list
+        $recipes = Recipe::select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'users.name')
+            ->join('users', 'users.id', '=', 'recipes.user_id')
+            ->orderBy('recipes.created_at', 'desc')
+            ->limit(3)
+            ->get();
+
+        $populars = Recipe::select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'users.name')
+            ->join('users', 'users.id', '=', 'recipes.user_id')
+            ->orderBy('recipes.views', 'desc')
+            ->limit(2)
+            ->get();
+
+        return view('home', compact('recipes', 'populars'));
+    }
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return view('home');
     }
 
     /**
